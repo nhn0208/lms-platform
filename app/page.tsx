b6860a07ca9362fd41e3be3398c944b05126c171
nhn28 <nguyenhoainam020803@gmail.com>
2023-09-28 11:01:52 +0700

    Authentication #2

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { currentUser } from "@clerk/nextjs";
-import type { User } from "@clerk/nextjs/api";
- 
-export default async function Page() {
-  const user: User | null = await currentUser();
-  // ...
-}
