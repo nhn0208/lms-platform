aef9bfa883f8a9bdf5e6a5dcefa9812d6d877346
nhn28 <nguyenhoainam020803@gmail.com>
2023-09-30 17:29:02 +0700

    categories

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,6 +1,23 @@
-const SearchPage = () => {
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+
+import { Categories } from "./_components/category"
+
+const SearchPage = async () => {
+  const { userId } = auth();
+  if (!userId) return redirect('/');
+
+  const categories = await db.category.findMany({
+    orderBy:{
+      name: "asc"
+    }
+  })
   return (
-    <div>SearchPage</div>
+    <div className="p-6">
+      <Categories items={categories} />
+    </div>
   )
 }
 
